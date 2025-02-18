0f8e7f68dea61db24402c04e89896be5a65f65b2
nmuni <nmunitizp@alumnos.nebrija.es>
2025-02-18 19:00:17 +0100

    Base de datos funcional

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hola fernado y nicolas ");
+        System.out.println("Hola fernado y nicolás");
+        System.out.println("La base de datos está funcionando");
     }
 }
